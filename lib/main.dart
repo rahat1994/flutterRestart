9042f9a479bae4a49e90055ed2abf5805a047013
Rahat Baksh <r.baksh@fusionenterprise.com>
2019-12-24 15:54:42 +0600

    adding assets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Hello Rahat'),
-      centerTitle: true,
-
-    ),
-    body: Center(
-      child: Text('Hello everyone'),
-    ),
-    floatingActionButton: FloatingActionButton(
-        child: Text('Click'),
-        onPressed: null
-    ),
-  ),
+  home: MyApp(),
 ));
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Rahat'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello everyone',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.black,
+            fontFamily: 'Girassol',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        onPressed: null,
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
